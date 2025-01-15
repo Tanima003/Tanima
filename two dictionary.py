35f3b23a5d637bfb31cdfa5f9b8a314ec04e1907
Tanima003 <162417811+Tanima003@users.noreply.github.com>
2025-01-15 15:50:44 +0530

    two dictionary.py

---
+++ two dictionary.py
@@ -0,0 +1,9 @@
+try:
+    dict1 = {'a': 1, 'b': 2}
+    dict2 = {'c': 3, 'd': 4}
+    merged_dict = dict1 | dict2
+
+    print("Merged Dictionary:", merged_dict)
+
+except (TypeError, ValueError) as e:
+    print(f"An error occurred: {e}")
